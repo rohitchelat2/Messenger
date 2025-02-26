b9cda7fd04c2f9d52937ad83ab151c0661ddfe1c
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2025-02-26 20:26:00 +0200

    cookie

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -15,8 +15,8 @@ if (Deno.env.get("JWT_SECRET")) {
 
 const getContacts = async (c) => {
     const token = getCookie(c, COOKIE_KEY);
-    console.log(token);
- 
+    console.log(c);
+    if(!token){return c.json({error:"No cookies"})}
     const jwtPayload = await jwt.verify(token, secret);
     
     const userID = jwtPayload.id;
