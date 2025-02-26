cf7bd50d27e1277fb48f5dac98aae2e5705c0910
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2025-02-26 19:23:24 +0200

    cookie_correction

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -3,6 +3,7 @@ import * as userService from "../services/userService.js"
 import * as messageService from "../services/messageService.js"
 import { getCookie } from  "@hono/hono/cookie";
 import * as jwt from "@hono/hono/jwt"
+import { consoleSize } from "https://deno.land/std@0.132.0/_deno_unstable.ts";
 let secret;
 const COOKIE_KEY = "auth";
 
@@ -14,7 +15,8 @@ if (Deno.env.get("JWT_SECRET")) {
 
 const getContacts = async (c) => {
     const token = getCookie(c, COOKIE_KEY);
-   // console.log(token);
+    console.log(token);
+    console.log(secret);
     const jwtPayload = await jwt.verify(token, secret);
     
     const userID = jwtPayload.id;
